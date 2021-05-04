95b3d2dd968769a2880aed20097942bc71f4bd33
Diego Alejandro Navichoque Pérez <78981597+DANavi721@users.noreply.github.com>
2021-05-04 10:08:36 -0600

    Revert "Desafío"

--- main.cpp
+++ main.cpp
@@ -1,17 +1,27 @@
+// <Diego Alejandro Navichoque Perez 1663721>
 #include <iostream>
-#include <string>
-
 using namespace std;
-
-int main()
-{
-    /*
-     * Escriba un programa que remueva los elementos duplicados de cualquier arreglo, debe mostrar en pantalla el arreglo resultante
-     * Ejemplo 1: array = [1,2,2,3] array2 = [2,2] ---> Resultado: [1,2,3]
-     * Ejemplo 2: array = [0,0,0,1,2,2,3,3,3,3,3,3,4,4,4] ---> Resultado: [0,1,2,3,4]
-     * Consejo: Asuma que el arreglo que se probará siempre va a estar ordenado
-     */
-    cout << "Challenge #1" << endl;
-    int arreglo[] = {0, 1, 1, 1, 2, 2};  // Empiece con este arreglo ya existente, el docente modificará este arreglo para pruebas
-    return 0;
+int main() {
+	int num;
+	int contador;
+	int sum;
+	int promedio;
+	contador = 0;
+	promedio = 0;
+	sum = 0;
+	do
+	{
+		cout << "ingrese un numero" << endl;
+		cin >> num;
+		if (contador > 0) {
+			contador = contador + 1;
+			sum = sum + num;
+			promedio = sum / contador;
+		}
+		else if (num < 0)
+		{
+			contador = contador - 1;
+		}
+	} while (num > 0);
+	cout << "El promedio de los numeros ingresados es de:" << promedio << endl;
 }
